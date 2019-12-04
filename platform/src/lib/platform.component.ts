e36bb19041ab7f3c0ab369ae8ea392fadbdd95ba
Andras Kovacs <elcsiga@gmail.com>
2019-12-04 12:45:47 +0100

    main3

---
+++ platform/src/lib/platform.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-platform',
+  template: `
+    <p>
+      platform works!
+    </p>
+  `,
+  styles: []
+})
+export class PlatformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ platform/src/lib/platform.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { PlatformComponent } from './platform.component';
+
+
+
+@NgModule({
+  declarations: [PlatformComponent],
+  imports: [
+  ],
+  exports: [PlatformComponent]
+})
+export class PlatformModule { }
